76dd21eecb02c99340d651b3845c8d4fd6fa2318
Kroney87 <143774147+Kroney87@users.noreply.github.com>
2023-09-15 12:30:22 -0400

    Add files via upload

--- ColeKronenwetter_Matlab1.m
+++ ColeKronenwetter_Matlab1.m
@@ -19,4 +19,12 @@ A = [1,-2,1,1,-1;-2,5,-3,1,0;1,-2,3,-4,4;1,2,0,3,1];
 x = rref(A)
 disp("There is a unique solution. x1 = 2, x2 = 1, x3 = 0, x4 = -1")
 %% Problem 2
+syms a b
+A = [12,5,a;20,4,b];
+rref(A)
+disp("The vector [a;b] can be expressed as a linear combination," ...
+    + newline + "therefore it must be in the span of S.")
 
+disp("c1 = 5b/52 - a/13")
+disp("c2 = 5a/13 - 3b/13")
+%%disp("[a;b] = [12;20](5b/52 - a/13) + [5;4](5a/13 - 3b/13)")
